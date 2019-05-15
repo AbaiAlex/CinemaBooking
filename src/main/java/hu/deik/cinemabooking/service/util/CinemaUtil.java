c75e3d1a2e64a019c09e8466b5541bf9189074fb
AbaiAlex <33180187+AbaiAlex@users.noreply.github.com>
2019-05-15 11:47:17 +0200

    Update CinemaUtil.java

--- src/main/java/hu/deik/cinemabooking/service/util/CinemaUtil.java
+++ src/main/java/hu/deik/cinemabooking/service/util/CinemaUtil.java
@@ -1,4 +1,32 @@
 package hu.deik.cinemabooking.service.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Segédmetódusokat tartalmazó osztály.
+ */
 public class CinemaUtil {
+
+    /**
+     * Logger.
+     */
+    private static Logger logger = LoggerFactory.getLogger(CinemaUtil.class);
+
+
+    /**
+     * Egy jegy fix ára.
+     */
+    private static final int EGY_JEGY_ARA = 1500;
+
+    /**
+     * Ár kalkulátor segédmetódus. Egy jegy árát a {@link #EGY_JEGY_ARA} határozza meg.
+     *
+     * @param jegyekSzama a felületen felvett jegyek száma
+     * @return az ár
+     */
+    public static int calculateAr(int jegyekSzama) {
+        logger.info("Calculating price");
+        return EGY_JEGY_ARA * jegyekSzama;
+    }
 }
